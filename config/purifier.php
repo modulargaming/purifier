c38fdd5101dd8f92c576917a11ca22058c32ceab
Curtis Delicata <curtis@modulargaming.com>
2013-01-01 10:53:55 +0000

    Update config

--- config/purifier.php
+++ config/purifier.php
@@ -7,6 +7,8 @@ return array(
 		/**
 		 * Use the application cache for HTML Purifier
 		 */
-		// 'Cache.SerializerPath' => APPPATH.'cache',
+	'HTML.Allowed' => 	'h1,h2,i,b,u,a[href],img[src],
+				blockquote[style],ul,ol,li,br',
+	'Cache.SerializerPath' => APPPATH.'cache',
 	),
 );
